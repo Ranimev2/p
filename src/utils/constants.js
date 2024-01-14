86ba03da8d6b89ac696a690005d2822ab1c984fc
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-01-14 21:19:42 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
 
-export const logo = "https://i.ibb.co/nLWFQM5/logo-me-1.png";
+export const logo = "https://i.ibb.co/Fx6BvNB/IMG-20240106-WA0379-removebg-preview.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
